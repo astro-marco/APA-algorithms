e29271db76d30d68909e9d03f9eb6a2f07ee61c6
Marco Mandelli <119368321+astro-marco@users.noreply.github.com>
2024-12-06 14:00:01 +0100

    Add main function and update LCS3_b

--- LCS3.py
+++ LCS3.py
@@ -1,13 +1,18 @@
 # type: ignore
 from tkinter.tix import MAX
 
+def main():
+	X = "casella"
+	Y = "rilevatore"
+	W = "cavolfiore"
+
 def LCS3_b(X,Y,W):
 	m = len(X)
 	n = len(Y)
 	l = len(W)
 	
     c = [[[0 for h in range(l+1)]for j in range(n+1)]for i in range(m+1)]
-    b = [[['' for h in range(l+1)]for j in range(n+1)]for i in range(m+1)]
+    b = [[[0 for h in range(l+1)]for j in range(n+1)]for i in range(m+1)]
 	
 	for i in range(0,m):
 		for h in range(0,l):
@@ -24,19 +29,30 @@ def LCS3_b(X,Y,W):
 	for i in range(1,m):
 		for j in range(1,n):
 			for h in range(1,l):
-				if X[i] == Y[j]:
+				# le 7 direzioni che "regrediscono" verso b[0][0][0]
+				# quelle possibili sarebbero 26 immaginando un cubo 3x3x3
+				if X[i] == Y[j] and Y[j] == W[h]:
+					c[i][j][h] = 1 + c[i-1][j-1][h-1]
+					b[i][j][h] = 2 # giù in diagonale (verso lo spigolo [0,0,0])
+				
+				elif X[i] == Y[j]:
 					c[i][j][h] = 1 + c[i-1][j-1][h]
-					b[i][j][h] = '↖' # giù diagonale 3D
+					b[i][j][h] = 5 # in diagonale orizzontale (verso [0,0,h])
+				
 				elif Y[j] == W[h]:
 					c[i][j][h] = 1 + c[i][j-1][h-1]
+					b[i][j][h] = 3 # giù a destra
+				
 				elif X[i] == W[h]:
 					c[i][j][h] = 1 + c[i-1][j][h-1]
+					b[i][j][h] = 1 # giù a sinistra
+
 				else:
 					c[i][j][h] = max(c[i-1][j][h], c[i][j-1][h], c[i][j][h-1])
 					if c[i][j][h] == c[i-1][j][h]:
-						b[i][j][h] = '↑'
+						b[i][j][h] = 4
 					elif c[i][j][h] == c[i][j-1][h]:
-						b[i][j][h] = '←'
+						b[i][j][h] = 6
 					elif c[i][j][h] == c[i][j][h-1]:
-						b[i][j][h] = '↑' # 
+						b[i][j][h] = 7 # 
 return c[m][n][l]
