a9aef3f28340ba60492c1e7222bb7fc4d5c40850
Marco Mandelli <119368321+astro-marco@users.noreply.github.com>
2024-12-10 19:42:45 +0100

    Fix indexing in LCS3_b function to correctly handle base cases and improve clarity

--- LCS3.py
+++ LCS3.py
@@ -14,21 +14,22 @@ def LCS3_b(X,Y,W):
 	c = [[[0 for h in range(l+1)]for j in range(n+1)]for i in range(m+1)]
 	b = [[[0 for h in range(l+1)]for j in range(n+1)]for i in range(m+1)]
 	
-	for i in range(0,m):
-		for h in range(0,l):
+	# Fill the base cases
+	for i in range(0,m+1):
+		for h in range(0,l+1):
 			c[i][0][h] = 0
 			
 	for j in range(1,n):
-		for h in range(0,l):
-				c[0][j][h] = 0
+		for h in range(0,l+1):
+			c[0][j][h] = 0
 
-	for i in range(1,m):
-		for j in range(1,n):
+	for i in range(1,m+1):
+		for j in range(1,n+1):
 			c[i][j][0] = 0
 
-	for i in range(1,m):
-		for j in range(1,n):
-			for h in range(1,l):
+	for i in range(1,m+1):
+		for j in range(1,n+1):
+			for h in range(1,l+1):
 				# The 7 directions that 'regress' towards position b[0][0][0].
 				# Total number of directions would be 26, pretending to stand
 				# in the central position of a 3x3x3 cube.
