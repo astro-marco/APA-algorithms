37d7984f7f97e8f6ab9eff84e8bd746f1b891380
Marco Mandelli <119368321+astro-marco@users.noreply.github.com>
2024-12-13 01:16:24 +0100

    Implement print_LCS3 function and update main to execute LCS3_b and display results

--- LCS3.py
+++ LCS3.py
@@ -1,7 +1,5 @@
 # type: ignore
 
-b = None
-
 def LCS3_b(X,Y,W):
 	global b
 	m = len(X)
@@ -56,7 +54,47 @@ def LCS3_b(X,Y,W):
 						b[i][j][h] = 7 # vertical down
 	return c[m][n][l]
 
-# to retrieve b from LCS3_b and use it in print_LCS3
-def get_b():
-    global b
-    return b
+def print_LCS3(b,X,Y,W):
+    i = len(X)
+    j = len(Y)
+    h = len(W)
+    invertedLCS3 = [] # list to store characters to print
+
+    # navigating the i x j x h cube (3D matrix)
+    while i != 0 and j != 0 and h != 0:
+        if b[i][j][h] == 1: # horizontal left
+            j -= 1
+        elif b[i][j][h] == 2: # horizontal diagonal
+            i -= 1
+            j -= 1
+        elif b[i][j][h] == 3: # horizontal right
+            i -= 1
+        elif b[i][j][h] == 4: # down-left
+            j -= 1
+            h -= 1
+        elif b[i][j][h] == 5: # down-diagonal
+            invertedLCS3.append(X[i-1])
+            i -= 1
+            j -= 1
+            h -= 1
+        elif b[i][j][h] == 6: # down-right
+            i -= 1
+            h -= 1
+        elif b[i][j][h] == 7: # vertical down
+            h -= 1
+		
+    # Reverse the list to get the correct order of LCS
+    LCS3 = ''.join(reversed(invertedLCS3))
+    print(LCS3)
+
+def main():
+    X = "cammello"
+    Y = "casa"
+    W = "padella"
+    
+    LCS3_b(X, Y, W)
+    print("Length of LCS is", LCS3_b(X,Y,W))
+    print("One of the LCSs is")
+    print_LCS3(b,X,Y,W)
+
+main() # per poter eseguire il file
