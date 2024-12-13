e27f3e365a069f91d55c9c5e4f7d8d45d4b88cb5
Marco Mandelli <119368321+astro-marco@users.noreply.github.com>
2024-12-13 01:45:28 +0100

    Refactor LCS3_b function for improved readability and debugging; adjust print_LCS3 logic for correct LCS retrieval

--- LCS3.py
+++ LCS3.py
@@ -2,59 +2,59 @@
 import pprint
 
 def LCS3_b(X,Y,W):
-	global b
-	m = len(X)
-	n = len(Y)
-	l = len(W)
-	
-	c = [[[0 for h in range(l+1)]for j in range(n+1)]for i in range(m+1)]
-	b = [[[0 for h in range(l+1)]for j in range(n+1)]for i in range(m+1)]
-	
-	# Fill the base cases
-	for i in range(m+1):
-		for h in range(l+1):
-			c[i][0][h] = 0
-			
-	for j in range(1,n+1):
-		for h in range(0,l+1):
-			c[0][j][h] = 0
+    global b
+    m = len(X)
+    n = len(Y)
+    l = len(W)
+    
+    c = [[[0 for h in range(l+1)]for j in range(n+1)]for i in range(m+1)]
+    b = [[[0 for h in range(l+1)]for j in range(n+1)]for i in range(m+1)]
+    
+    # Fill the base cases
+    for i in range(m+1):
+        for h in range(l+1):
+            c[i][0][h] = 0
+            
+    for j in range(1,n+1):
+        for h in range(0,l+1):
+            c[0][j][h] = 0
 
-	for i in range(1,m+1):
-		for j in range(1,n+1):
-			c[i][j][0] = 0
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            c[i][j][0] = 0
 
-	for i in range(1,m+1):
-		for j in range(1,n+1):
-			for h in range(1,l+1):
-				# The 7 directions that 'regress' towards position b[0][0][0].
-				# Total number of directions would be 26, pretending to stand
-				# in the central position of a 3x3x3 cube.
-				if X[i-1] == Y[j-1] and Y[j-1] == W[h-1]:
-					print(f"Match found at ({i}, {j}, {h}): {X[i-1]}")  # Debug
-					c[i][j][h] = 1 + c[i-1][j-1][h-1]
-					b[i][j][h] = 5 # down-diagonal (ideally towards [0,0,0])
-				
-				elif X[i-1] == Y[j-1]:
-					c[i][j][h] = 1 + c[i-1][j-1][h]
-					b[i][j][h] = 2 # horizontal diagonal (towards [0,0,h])
-				
-				elif Y[j-1] == W[h-1]:
-					c[i][j][h] = 1 + c[i-1][j][h-1]
-					b[i][j][h] = 6 # down-right
-				
-				elif X[i-1] == W[h-1]:
-					c[i][j][h] = 1 + c[i][j-1][h-1]
-					b[i][j][h] = 4 # down-left
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            for h in range(1,l+1):
+                # The 7 directions that 'regress' towards position b[0][0][0].
+                # Total number of directions would be 26, pretending to stand
+                # in the central position of a 3x3x3 cube.
+                if X[i-1] == Y[j-1] and Y[j-1] == W[h-1]:
+                    print(f"Match found at ({i}, {j}, {h}): {X[i-1]}")  # Debug
+                    c[i][j][h] = 1 + c[i-1][j-1][h-1]
+                    b[i][j][h] = 5 # down-diagonal (ideally towards [0,0,0])
+                
+                elif X[i-1] == Y[j-1]:
+                    c[i][j][h] = 1 + c[i-1][j-1][h]
+                    b[i][j][h] = 2 # horizontal diagonal (towards [0,0,h])
+                
+                elif Y[j-1] == W[h-1]:
+                    c[i][j][h] = 1 + c[i-1][j][h-1]
+                    b[i][j][h] = 6 # down-right
+                
+                elif X[i-1] == W[h-1]:
+                    c[i][j][h] = 1 + c[i][j-1][h-1]
+                    b[i][j][h] = 4 # down-left
 
-				else:
-					c[i][j][h] = max(c[i-1][j][h], c[i][j-1][h], c[i][j][h-1])
-					if c[i][j][h] == c[i-1][j][h]:
-						b[i][j][h] = 3 # horizontal right
-					elif c[i][j][h] == c[i][j-1][h]:
-						b[i][j][h] = 1 # horizontal left
-					elif c[i][j][h] == c[i][j][h-1]:
-						b[i][j][h] = 7 # vertical down
-	return c[m][n][l]
+                else:
+                    c[i][j][h] = max(c[i-1][j][h], c[i][j-1][h], c[i][j][h-1])
+                    if c[i][j][h] == c[i-1][j][h]:
+                        b[i][j][h] = 3 # horizontal right
+                    elif c[i][j][h] == c[i][j-1][h]:
+                        b[i][j][h] = 1 # horizontal left
+                    elif c[i][j][h] == c[i][j][h-1]:
+                        b[i][j][h] = 7 # vertical down
+    return c[m][n][l]
 
 def print_LCS3(b,X,Y,W):
     i = len(X)
@@ -65,7 +65,14 @@ def print_LCS3(b,X,Y,W):
     # navigating the i x j x h cube (3D matrix)
     while i > 0 and j > 0 and h > 0:
         print(f"Position: ({i}, {j}, {h}), b[i][j][h]: {b[i][j][h]}")  # Debug
-        if b[i][j][h] == 1: # horizontal left
+        
+        # this condition MUST be checked before all the other ones!!!
+        if b[i][j][h] == 5: # down-diagonal
+            invertedLCS3.append(X[i-1])
+            i -= 1
+            j -= 1
+            h -= 1
+        elif b[i][j][h] == 1: # horizontal left
             j -= 1
         elif b[i][j][h] == 2: # horizontal diagonal
             i -= 1
@@ -73,11 +80,6 @@ def print_LCS3(b,X,Y,W):
         elif b[i][j][h] == 3: # horizontal right
             i -= 1
         elif b[i][j][h] == 4: # down-left
-            j -= 1
-            h -= 1
-        elif b[i][j][h] == 5: # down-diagonal
-            invertedLCS3.append(X[i-1])
-            i -= 1
             j -= 1
             h -= 1
         elif b[i][j][h] == 6: # down-right
@@ -88,15 +90,15 @@ def print_LCS3(b,X,Y,W):
         else:
             print("Unexpected value in b:", b[i][j][h])  # Debug
             break  # Interrompi in caso di valori non previsti
-		
+        
     # Reverse the list to get the correct order of LCS
     LCS3 = ''.join(reversed(invertedLCS3))
     print(LCS3)
 
 def main():
-    X = "cammello"
-    Y = "casa"
-    W = "padella"
+    X = "abc"
+    Y = "abc"
+    W = "abc"
     
     print("Length of LCS is", LCS3_b(X,Y,W))
     pprint.pprint(b) # Print the matrix `b` for debugging
